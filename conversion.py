86b731bacf0b7d53615e4bb181ae456e3abeddca
vedantsingh23 <141285386+vedantsingh23@users.noreply.github.com>
2023-09-22 16:52:23 -0700

    Update conversion.py

--- conversion.py
+++ conversion.py
@@ -1,3 +1,7 @@
 decimal = bin(int(input("Enter a decimal number: ")))
 decimal = decimal.replace("0b","")
 print(decimal)
+
+decimall = hex(int(input("Enter a decimal number: ")))
+decimall = decimall.replace("0x","")
+print(decimall)
